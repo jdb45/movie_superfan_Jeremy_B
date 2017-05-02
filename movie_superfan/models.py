8d5772e199994b30ca74d108dc6ae5bf95714a03
Jeremy <acnajcadcac.yahoo.com>
2017-05-02 13:03:20 -0500

    Added postgres settings. Created a home.html file, it doesnt do anything yet. Added user data to the model. Created a user registration form.

--- movie_superfan/models.py
+++ movie_superfan/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+User._meta.get_field('email')._unique = True
+
+User._meta.get_field('email')._blank = False
+User._meta.get_field('last_name')._blank = False
+User._meta.get_field('first_name')._blank = False
